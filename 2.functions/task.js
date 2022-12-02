e8e84e65e9d0f4ff06be01b783ad8d9af5fae3c6
AnnaSt <anutabatuk@gmail.com>
2022-12-02 15:08:31 +0800

    first commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,62 @@
+
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
-
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+    for (let i = 0; i < arr.length; i += 1) {
+      if(arr[i] < min)
+        min = arr[i]; 
+     
+     if(arr[i] > max)
+        max = arr[i];
+      
+      sum += arr[i];
+    
+    }
+    avg = +(sum / arr.length).toFixed(2);
+    
+    
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+      sum += arr[i];
+      }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const sum = func(arrOfArr[i]);
+    if (sum > max) {
+      max = sum;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, difference;
+
+  min = Infinity;
+  max = -Infinity;
+  for (let i = 0; i < arr.length; i += 1) {
+    if(arr[i] < min)
+      min = arr[i]; 
+   
+   if(arr[i] > max)
+      max = arr[i];
+     difference = Math.abs(min - max); 
+  }
+  return difference;
+
 }
+
