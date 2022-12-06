862ecb0ab73c0d05cdeab8d2b5e3e16b7f40ece0
AnnaSt <anutabatuk@gmail.com>
2022-12-06 09:08:49 +0800

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
-  return result; // boolean
+ return result = (arr1.length === arr2.length) && (arr1.every((elem, index) => arr1[elem,index] === arr2[elem,index]));
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(elem => elem % 3 == 0 ).filter(elem => elem > 0).map((elem) => elem * 10);
   return resultArr; // array
 }
